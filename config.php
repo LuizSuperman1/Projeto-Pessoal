a1f30e971cc7e9a04c028a437ddc49be9dce08c4
Luiz Eduardo <101329633+LuizSuperman1@users.noreply.github.com>
2023-06-25 19:03:08 -0300

    cadastro

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'Luiz1502*';
+    $dbName = 'cadastro-user';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName)
+
+    //if($conexao->connect_erro)
+    //{
+
+      //  echo "Erro"
+
+    //}
+    //else
+    //{
+
+       // echo "Conexçao efetuada com sucesso"
+
+    //}
+
+    
+
+?>
